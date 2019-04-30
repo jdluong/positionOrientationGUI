130fefc3ae5ae223fa0b03b42efb2c937179ac77
gaelias <gelias@uci.edu>
2019-04-30 15:34:35 -0700

    Update OrientationData PROTO.m

--- OrientationData PROTO.m
+++ OrientationData PROTO.m
@@ -1,5 +1,49 @@
+%%
+portPosX = strcmp(orientMatrixColIDs, 'PortX');
+portPosY = strcmp(orientMatrixColIDs, 'PortY');
+headPosX = strcmp(orientMatrixColIDs, 'HeadX');
+headPosY = strcmp(orientMatrixColIDs, 'HeadY');
+tailPosX = strcmp(orientMatrixColIDs, 'TailX');
+tailPosY = strcmp(orientMatrixColIDs, 'TailY');
+
+portAngle = nan(size(orientMatrix,1),1);
+headAngle = nan(size(orientMatrix,1),1);
+tailAngle = nan(size(orientMatrix,1),1);
+
+htVal = nan(size(orientMatrix,1),1);
+hpVal = nan(size(orientMatrix,1),1);
+ptVal = nan(size(orientMatrix,1),1);
+
+posIndices = find(~isnan(orientMatrix(:,2)));
+
+for pos = 1:length(posIndices)
+    curPosNdx = posIndices(pos);
+    curPortX = orientMatrix(curPosNdx, portPosX);
+    curPortY = orientMatrix(curPosNdx, portPosY);
+    curHeadX = orientMatrix(curPosNdx, headPosX);
+    curHeadY = orientMatrix(curPosNdx, headPosY);
+    curTailX = orientMatrix(curPosNdx, tailPosX);
+    curTailY = orientMatrix(curPosNdx, tailPosY);
+    
+    htVal(curPosNdx) = sqrt((curTailX - curHeadX)^2 + (curTailY - curHeadY)^2);
+    hpVal(curPosNdx) = sqrt((curPortX - curHeadX)^2 + (curPortY - curHeadY)^2);
+    ptVal(curPosNdx) = sqrt((curTailX - curPortX)^2 + (curTailY - curPortY)^2);
+    
+    portAngle(curPosNdx) = rad2deg(acos((ptVal(curPosNdx)^2 + hpVal(curPosNdx)^2 - htVal(curPosNdx)^2)/(2*ptVal(curPosNdx)*hpVal(curPosNdx))));
+    headAngle(curPosNdx) = rad2deg(acos((htVal(curPosNdx)^2 + hpVal(curPosNdx)^2 - ptVal(curPosNdx)^2)/(2*htVal(curPosNdx)*hpVal(curPosNdx))));
+    tailAngle(curPosNdx) = rad2deg(acos((htVal(curPosNdx)^2 + ptVal(curPosNdx)^2 - hpVal(curPosNdx)^2)/(2*htVal(curPosNdx)*ptVal(curPosNdx))));    
+end
+
+orientMatrix = [orientMatrix, portAngle, headAngle, tailAngle, htVal, hpVal, ptVal];
+orientMatrixColIDs = [orientMatrixColIDs, {'PortAngle'}, {'HeadAngle'}, {'TailAngle'}, {'HeadTailLength'}, {'HeadPortLength'}, {'PortTailLength'}];
+
+%%
 preTrialBehavMatrix = OrganizeTrialData_SM(behavMatrix, behavMatrixColIDs, [-0.25 1.2], 'PokeIn');
-ptOrient = ExtractTrialData_SM(preTrialBehavMatrix, orientMatrix(:,2:end)); %#ok<*NODEF>
+pokeInTimes = behavMatrix([preTrialBehavMatrix.PokeInIndex],1);
+ptOrient = ExtractTrialData_SM(preTrialBehavMatrix, orientMatrix); %#ok<*NODEF>
+for trl = 1:length(ptOrient)
+    ptOrient{trl}(:,1) = ptOrient{trl}(:,1)-pokeInTimes(trl);
+end
 
 corrTrlLog = [preTrialBehavMatrix.Performance];
 isLog = [preTrialBehavMatrix.TranspositionDistance]==0;
@@ -8,74 +52,19 @@ for o = 1:4
     odorLog(o,:) = [preTrialBehavMatrix.Odor]==o;
 end
 
+
 %%
 figure;
 sps = nan(1,4);
 for o = 1:4
     curOrientOdor = cell2mat(ptOrient(odorLog(o,:) & corrTrlLog & isLog)');
     sps(o) = subplot(1,4,o);
-    headX = curOrientOdor(~isnan(curOrientOdor(:,3)),3);
-    headY = curOrientOdor(~isnan(curOrientOdor(:,4)),4);
+    headX = curOrientOdor(~isnan(curOrientOdor(:,4)),4);
+    headY = curOrientOdor(~isnan(curOrientOdor(:,5)),5);
     scatter(headX, headY, 'b');
     hold on;
-    tailX = curOrientOdor(~isnan(curOrientOdor(:,5)),5);
-    tailY = curOrientOdor(~isnan(curOrientOdor(:,6)),6);
+    tailX = curOrientOdor(~isnan(curOrientOdor(:,6)),6);
+    tailY = curOrientOdor(~isnan(curOrientOdor(:,7)),7);
     scatter(tailX, tailY, 'r');
 end
 linkaxes(sps, 'xy');
-
-%%
-xMax = max(get(sps(1), 'xlim'));
-headXcol = strcmp(orientMatrixColIDs, 'HeadX');
-tailXcol = strcmp(orientMatrixColIDs, 'TailX');
-yMax = max(get(sps(1), 'ylim'));
-headYcol = strcmp(orientMatrixColIDs, 'HeadY');
-tailYcol = strcmp(orientMatrixColIDs, 'TailY');
-
-numUnis = length(ensembleUnitSummaries);
-% Gaussian used for calculating instantaneous firing rate
-slideWindowSize = 200;
-instFRgauss = gausswin(slideWindowSize);
-instFRgauss = instFRgauss/(length(instFRgauss)*mode(diff(behavMatrix(:,1))));
-
-uniInstFR = nan(size(ensembleMatrix,1), size(ensembleMatrix,2)-1);
-for uni = 2:size(ensembleMatrix,2)
-    uniInstFR(:,uni-1) = conv(ensembleMatrix(:,uni), instFRgauss, 'same');
-end
-
-headFRmap = nan(yMax, xMax, numUnis);
-tailFRmap = nan(yMax, xMax, numUnis);
-
-orientMatrix = round(orientMatrix);
-xPosS = unique([orientMatrix(~isnan(orientMatrix(:,headXcol)),headXcol), orientMatrix(~isnan(orientMatrix(:,tailXcol)),tailXcol)]);
-yPosS = unique([orientMatrix(~isnan(orientMatrix(:,headYcol)),headYcol), orientMatrix(~isnan(orientMatrix(:,tailYcol)),tailYcol)]);
-
-for uni = 9:numUnis
-    tic
-    for x = 1:length(xPosS)
-        curX = xPosS(x);
-        for y = 1:length(yPosS)
-            curY = yPosS(y);
-            headPosMask = orientMatrix(:,headXcol)==curX & orientMatrix(:,headYcol)==curY;
-            headFRmap(curY,curX,uni) = mean(uniInstFR(headPosMask, uni));
-            tailPosMask = orientMatrix(:,tailXcol)==curX & orientMatrix(:,tailYcol)==curY;
-            tailFRmap(curY,curX,uni) = mean(uniInstFR(tailPosMask, uni));
-        end
-    end
-    
-    figure;
-    sp1 = subplot(1,2,1);
-    imagesc(headFRmap(:,:,uni), [0 max([max(max(headFRmap(:,:,uni))), max(max(tailFRmap(:,:,uni)))])]);
-    set(sp1, 'xlim', get(sps(1), 'xlim'), 'ylim', get(sps(1), 'ylim'), 'ydir', 'normal');
-    title(sprintf('%s Head', ensembleMatrixColIDs{uni+1}));
-    sp2 = subplot(1,2,2);
-    imagesc(tailFRmap(:,:,uni), [0 max([max(max(headFRmap(:,:,uni))), max(max(tailFRmap(:,:,uni)))])]);
-    set(sp2, 'xlim', get(sps(1), 'xlim'), 'ylim', get(sps(1), 'ylim'), 'ydir', 'normal');
-    title(sprintf('%s Tail', ensembleMatrixColIDs{uni+1}));
-    cmap = jet;
-    cmap(1,:) = [1 1 1];
-    colormap(cmap);
-    toc
-    drawnow
-end
-        
